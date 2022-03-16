c43bf2782a52bd24893a15f55343b9d2e85d6654
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2022-03-16 16:03:52 +0545

    added models and tests for models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.TextField(default="")
+    body = models.TextField(default="")
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,33 @@
+import imp
 from urllib import response
 from django.test import TestCase
+from blog.models import Blog
 
 # Create your tests here.
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Blog()
+        first_item.title = "the first ever blog"
+        first_item.body = "the first ever blog body"
+        first_item.save()
+
+        second_item = Blog()
+        second_item.title = "the second blog"
+        second_item.body = "the second blog body"
+        second_item.save()
+
+        saved_items = Blog.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.title, "the first ever blog")
+        self.assertEqual(first_saved_item.body, "the first ever blog body")
+        self.assertEqual(second_saved_item.title, "the second blog")
+        self.assertEqual(second_saved_item.body, "the second blog body")
 
 
 class NewItemTest(TestCase):
